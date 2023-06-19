67526375dee97f871ec9d98101f4bafc91a9d31b
Ivan Von Staden <ivanv@bbd.co.za>
2023-06-19 14:32:09 +0200

    Small fix

--- strategies/google.js
+++ strategies/google.js
@@ -1,10 +1,5 @@
 const passport = require("passport")
 const googleOauth = require("passport-google-oauth20")
-const path = require('path');
-const fs = require('fs');
-const configPath = path.join(__dirname, '../../secrets/config.json');
-const configData = fs.readFileSync(configPath, 'utf8');
-const config = JSON.parse(configData).google;
 const jwt = require("../controllers/jwtControl")
 const secrets = require("../controllers/secretControl");
 const secret_name = "prod/app/google"
