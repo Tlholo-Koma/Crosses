840958df179d531074d8daab5160496b1e575adf
Ivan Von Staden <ivanv@bbd.co.za>
2023-06-20 18:18:17 +0200

    Fixed Normal auth for deployment

---
+++ resource/routes/loginRoute.js
@@ -0,0 +1,132 @@
+const express = require("express");
+const passport = require("passport");
+const path = require("path");
+const loginRouter = express.Router();
+const auth = require("../controllers/authControl");
+const { default: axios } = require("axios");
+
+loginRouter.get("/", async function (req, res) {
+  res.sendFile(path.join(__dirname, "../pages/login.html"));
+});
+
+loginRouter.get(
+  "/google",
+  passport.authenticate("google"),
+  async function (req, res) {
+    res.send(200);
+  }
+);
+
+loginRouter.get(
+  "/google/redirect",
+  passport.authenticate("google", {
+    failureRedirect: `/login`,
+    session: false,
+  }),
+  async (req, res) => {
+    const email = req.user;
+    try {
+      const requestData = {
+        email: email,
+        socialLogin: true,
+        // other data you want to include in the request body
+      };
+      const response = await axios.post(
+        `${process.env.AUTHSERVER}/login/token`,
+        requestData
+      );
+      const token = response.data;
+      res.redirect(`/login?token=${token}`);
+      // Process the response data here
+    } catch (error) {
+      console.error(error);
+      // Handle the error here
+    }
+  }
+);
+
+loginRouter.get(
+  "/github",
+  passport.authenticate("github", { scope: ["user:email"] })
+);
+
+loginRouter.get(
+  "/github/redirect",
+  passport.authenticate("github", {
+    failureRedirect: `/login`,
+    session: false,
+  }),
+  async (req, res) => {
+    const email = req.user;
+    try {
+      const requestData = {
+        email: email,
+        socialLogin: true,
+        // other data you want to include in the request body
+      };
+      const response = await axios.post(
+        `${process.env.AUTHSERVER}/login/token`,
+        requestData
+      );
+      const token = response.data;
+      res.redirect(`/login?token=${token}`);
+      // Process the response data here
+    } catch (error) {
+      console.error(error);
+      // Handle the error here
+    }
+  }
+);
+
+loginRouter.post("/token", async function (req, res) {
+  console.log("Hit token");
+  const username = req.body.email;
+  console.log(username);
+  try {
+    const requestData = {
+      email: username,
+      socialLogin: false,
+      // other data you want to include in the request body
+    };
+    const response = await axios.post(
+      `${process.env.AUTHSERVER}/login/token`,
+      requestData
+    );
+    const token = response.data;
+    if (token === null) {
+      res.send(200);
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle the error here
+    res.send(500);
+  }
+});
+
+loginRouter.post("/verify", async function (req, res) {
+  console.log("Hit token");
+  const token = req.body.token;
+  console.log(token);
+  try {
+    const requestData = {
+      token: token
+      // other data you want to include in the request body
+    };
+    const response = await axios.post(
+      `${process.env.AUTHSERVER}/login/verify`,
+      requestData
+    );
+    const result = response.data;
+    if (result === true) {
+      res.send(200);
+    }else{
+      res.send(403)
+    }
+  } catch (error) {
+    console.error(error);
+    // Handle the error here
+    res.send(500);
+  }
+});
+
+module.exports = loginRouter;
---
+++ resource/scripts/login.js
@@ -0,0 +1,66 @@
+const baseUrl = window.location.href.split("/").slice(0, 3).join("/");
+const authUrl = baseUrl.replace("3001", "3000");
+const loginButton = document.getElementById("loginButton");
+const googleLoginButton = document.getElementById("googleLoginButton");
+const githubLoginButton = document.getElementById("githubLoginButton");
+const usernameInput = document.getElementById("username");
+
+googleLoginButton.addEventListener("click", async () => {
+    window.location.href = '/Login/google';
+});
+
+githubLoginButton.addEventListener("click", async () => {
+    window.location.href='/Login/github'
+});
+
+loginButton.addEventListener("click", async () => {
+  const user = usernameInput.value;
+  const data = {
+    email: user,
+  };
+
+  try {
+    let storedToken = localStorage.getItem("jwtToken") || null;
+    if (!storedToken) {
+      await getToken(data);
+      storedToken = localStorage.getItem("jwtToken");
+    }
+    checkToken(storedToken);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const checkToken = async (token) => {
+  const data = { token: token };
+  const response = await fetch(`${baseUrl}/login/verify`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const message = await response.json();
+  } else {
+    throw new Error("Error: " + response.status);
+  }
+};
+
+const getToken = async (data) => {
+  const response = await fetch(`${baseUrl}/login/token`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (response.ok) {
+    const token = await response.json();
+    localStorage.setItem("jwtToken", token);
+  } else {
+    throw new Error("Error: " + response.status);
+  }
+};
