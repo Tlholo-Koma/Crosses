c519da1cf0ac29bd1130c3d8aed1da274ccb6f89
DanielSBBD <daniels@bbd.co.za>
2023-06-20 23:24:35 +0200

    Added authorized fetch method

---
+++ resource/scripts/utils.js
@@ -0,0 +1,14 @@
+// This could be split up into authorizedGet, authorizedPost etc
+// We should use this to replace all use of axios
+export const authorizedFetch = async (url, method, token, body) => {
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      Authentication: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: body,
+  });
+
+  return response.json();
+};
