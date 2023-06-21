66284f61a9043db9f7495998b5175222ff179db5
Hyla Fourie <hyla@bbd.co.za>
2023-06-21 17:49:25 +0200

    logic in place, more styling required

---
+++ resource/scripts/game.js
@@ -0,0 +1,77 @@
+let currentPlayer = 'O';
+
+function handleClick(button) {
+  if (button.innerText !== '') {
+    return;
+  }
+  button.innerText = currentPlayer;
+  const currentPlayerDisplay = document.querySelector('.current-player');
+  currentPlayerDisplay.innerText = currentPlayer === 'O' ? 'X' : 'O';
+
+  if (checkWinner()) {
+    console.log('Player ' + currentPlayer + ' wins!');
+    resetBoard();
+  } else if (checkTie()) {
+    console.log('It\'s a tie!');
+    resetBoard();
+  } else {
+    currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
+  }
+}
+
+function resetBoard() {
+  const buttons = document.querySelectorAll('button');
+  for (const button of buttons) {
+    button.innerText = ''; // Clear the button text
+    button.dataset.state = ''; // Reset the data-state attribute
+  }
+  const currentPlayerDisplay = document.querySelector('.current-player');
+  currentPlayerDisplay.innerText = 'O';
+  currentPlayer = 'O'; // Reset the current player to 'O'
+}
+
+
+function checkWinner() {
+  const winningCombinations = [
+    // Rows
+    ['top-left', 'top-middle', 'top-right'],
+    ['middle-left', 'center', 'middle-right'],
+    ['bottom-left', 'bottom-middle', 'bottom-right'],
+    // Columns
+    ['top-left', 'middle-left', 'bottom-left'],
+    ['top-middle', 'center', 'bottom-middle'],
+    ['top-right', 'middle-right', 'bottom-right'],
+    // Diagonals
+    ['top-left', 'center', 'bottom-right'],
+    ['top-right', 'center', 'bottom-left']
+  ];
+
+  for (const combination of winningCombinations) {
+    const [cell1, cell2, cell3] = combination;
+    const button1 = document.getElementById(cell1);
+    const button2 = document.getElementById(cell2);
+    const button3 = document.getElementById(cell3);
+
+    if (
+      button1.innerText === currentPlayer &&
+      button2.innerText === currentPlayer &&
+      button3.innerText === currentPlayer
+    ) {
+      return true;
+    }
+  }
+
+  return false; // No winner
+}
+
+function checkTie() {
+  const buttons = document.querySelectorAll('button');
+
+  for (const button of buttons) {
+    if (button.innerText === '') {
+      return false;
+    }
+  }
+
+  return true;
+}
