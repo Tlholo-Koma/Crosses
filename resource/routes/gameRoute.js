382cc3a295ac219802b3b42ef6cd6651c3ced22c
Ivan Von Staden <ivanv@bbd.co.za>
2023-06-23 14:31:07 +0200

    Updated references

--- resource/routes/gameRoute.js
+++ resource/routes/gameRoute.js
@@ -7,4 +7,8 @@ gameRouter.get("/",auth.validateAuth, async function (req, res) {
     res.sendFile(path.join(__dirname, "../pages/game.html"));
 });
 
+gameRouter.get("/history",auth.validateAuth,async function (req,res) {
+    res.sendFile(path.join(__dirname, "../pages/history.html"));
+});
+
 module.exports = gameRouter;
